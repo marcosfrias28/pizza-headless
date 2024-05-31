353914fd948f22021bae0d20e44aaf10ec0a0ce6
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-31 17:31:04 +0200

    chore: Add endpoint for root path with Hello World message

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const express = require('express');
 const app = express();
 
 app.disable('x-powered-by'); // Disable the x-powered-by header
+
+
+app.get ('/', (req, res) => {
+    res.json({messsage: 'Hello World!'});
+    
+})
