4b3f030b553b99095e828fdde0f4d3080013db2c
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-06-01 14:48:25 +0200

    chore: Update express to use ES modules and add lazy loading for images

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const express = require('express');
-
-
+import express from 'express';
 const app = express();
+const PORT = process.env.PORT || 1234;
 
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`);
+})
 app.disable('x-powered-by'); // Disable the x-powered-by header
 
+app.use(express.json())
 
 app.get ('/', (req, res) => {
     res.json({messsage: 'Hello World!'});
-    
+})
+
+app.get ('/pizza', (req, res) => {
+    res.json({messsage: 'Tutte le pizze!'});
+})
+
+app.use('', (req,res) => {
+    res.status(404).send('<h1>404 NOT FOUND<h1/>Route not found');
 })
