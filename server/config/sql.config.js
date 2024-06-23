d008d8c677c3d1107deed18e56e0373b8f559d4d
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-06-23 18:49:45 +0200

    refactor: Update MySQL connection handling

--- server/config/sql.config.js
+++ server/config/sql.config.js
@@ -2,13 +2,12 @@ import dotenv from 'dotenv';
 import { createConnection } from 'mysql2/promise';
 dotenv.config();
 
-
 const MYSQL_CONFIG = {
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 }
 const connection = await createConnection(MYSQL_CONFIG);
 
