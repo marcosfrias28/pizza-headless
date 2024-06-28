fd7121784b2cfc4ed56aed1f70183239ca38b92d
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-06-28 18:51:26 +0200

    Tables Users, Pizza, Ingredient, PizzaIngredient defined

--- client/db/config.ts
+++ client/db/config.ts
@@ -1,6 +1,40 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
+
+const Users = defineTable({
+  columns: {
+    id: column.text({primaryKey: true, unique: true, nullable: false}),
+    name: column.text(),
+    email: column.text({ unique: true, nullable: false}),
+    password: column.text({ unique: true, nullable: false})
+  }
+})
+
+const Pizza = defineTable({
+  columns: {
+    id: column.text({primaryKey: true, unique: true, nullable: false}),
+    name: column.text(),
+    price: column.number(),
+    cover: column.text()
+  }
+})
+
+const Ingredient = defineTable({
+  columns: {
+    id: column.text({primaryKey: true, unique: true, nullable: false}),
+    name: column.text()
+  }
+})
+
+const PizzaIngredient = defineTable({
+  columns: {
+    pizza_id: column.text(),
+    ingredient_id: column.text()
+  }
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    Users, Pizza, Ingredient, PizzaIngredient
+  }
 });
