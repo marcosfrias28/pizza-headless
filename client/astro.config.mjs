0812e45baac104005873c6850a5f67842ffd4a29
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-06-28 18:36:40 +0200

    Implementing Astro DB instead of mysql server

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
-
 import auth from "auth-astro";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), auth()],
+  integrations: [tailwind(), react(), auth(), db()],
   output: 'server',
   adapter: vercel({
     edgeMiddleware: true
   }),
-  prefetch: true,
+  prefetch: true
 });
