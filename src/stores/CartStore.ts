cbb10b499c9818bc28012ac15188bbe5f476acf0
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-07-09 03:57:06 +0200

    And most important thiing, Store now is getted by useCartStore to avoid re-renders on the application

--- src/stores/CartStore.ts
+++ src/stores/CartStore.ts
@@ -2,14 +2,14 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 
-interface Item {
-  id: number
+export interface Item {
+  id: string
   name: string
   price: number
   quantity: number
 }
 
-type Cart = Item[]
+export type Cart = Item[]
 
 interface CartState {
   isCartOpen: boolean
@@ -18,15 +18,14 @@ interface CartState {
 
 interface ICartStore {
   cart: Cart
-  setCart : (cart: Cart) => void
+  setCart: (cart: Cart) => void
   clearCart: () => void
 }
 
-
-//NO PERSIST STATE, HANDLE IF CART IS OPEN OR NOT
+// NO PERSIST STATE, HANDLE IF CART IS OPEN OR NOT
 const CartState = create<CartState>((set) => ({
   isCartOpen: false,
-  setIsCartOpen: (isCartOpen: boolean) => set({isCartOpen}),
+  setIsCartOpen: (isCartOpen: boolean) => set({ isCartOpen })
 }))
 
 const CartStore = create(persist<ICartStore>((set) => ({
@@ -35,20 +34,39 @@ const CartStore = create(persist<ICartStore>((set) => ({
   clearCart: () => set({ cart: [] })
 }), { name: 'cart-store' }))
 
-
 const useCartStore = () => {
-  const {setCart } = CartStore(state => ({ setCart: state.setCart }))
-  const {clearCart} = CartStore(state => ({ clearCart: state.clearCart }))
-  const {cart} = CartStore(state => ({ cart: state.cart }))
-  const {isCartOpen} = CartState(state => ({ isCartOpen: state.isCartOpen, setIsCartOpen: state.setIsCartOpen}))
-  const {setIsCartOpen} = CartState(state => ({ setIsCartOpen: state.setIsCartOpen }))
-return {
-  setCart,
-  clearCart,
-  cart,
-  isCartOpen,
-  setIsCartOpen
-}
+  const { setCart } = CartStore(state => ({ setCart: state.setCart }))
+  const { clearCart } = CartStore(state => ({ clearCart: state.clearCart }))
+  const { cart } = CartStore(state => ({ cart: state.cart }))
+  const { isCartOpen } = CartState(state => ({ isCartOpen: state.isCartOpen, setIsCartOpen: state.setIsCartOpen }))
+  const { setIsCartOpen } = CartState(state => ({ setIsCartOpen: state.setIsCartOpen }))
+
+  const handleIncrement = (id: string) => {
+    setCart(cart.map((item) => (item.id === id ? { ...item, quantity: item.quantity + 1 } : item)))
+  }
+  const handleDecrement = (id: string) => {
+    const item = cart.filter(item => item.id === id)
+    if (item[0]) { // if item is found in cart and quantity is 1, remove it
+      if (item[0].quantity === 1) return handleRemove(id)
+      setCart(cart.map((item) => (item.id === id && item.quantity > 1 ? { ...item, quantity: item.quantity - 1 } : item)))
+      }
+  }
+  const handleRemove = (id: string) => {
+    setCart(cart.filter((item) => item.id !== id))
+  }
+  const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0)
+
+  return {
+    handleIncrement,
+    handleDecrement,
+    handleRemove,
+    setCart,
+    clearCart,
+    cart,
+    isCartOpen,
+    total,
+    setIsCartOpen
+  }
 }
 
 export default useCartStore
